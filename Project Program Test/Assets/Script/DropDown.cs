9f1ef1c34b93eaf9190cb138ee6774588ed32413
Zivadino <danielthomsen2013@gmail.com>
2021-12-29 15:39:15 +0100

    Dropdown script

---
+++ Project Program Test/Assets/Script/DropDown.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class DropDown : MonoBehaviour
+{
+    public TextMeshProUGUI output;
+
+    public SimpleRandomWalkData BigDungeon;
+    public GameObject CorridorFirst;
+    
+    public void HandleInputData(int val)
+    {
+        if (val == 0)
+        {
+            Debug.Log("Is this working");
+            
+        }
+
+        if (val == 1)
+        {
+            
+        }
+
+        if (val == 2)
+        {
+            
+        }
+
+        if (val == 3)
+        {
+            
+        }
+
+        if (val == 4)
+        {
+            
+        }
+        
+    }
+
+}
